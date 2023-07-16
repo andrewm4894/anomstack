e8a673518816a6e4d5583b2901d13a3b9db750d8
Andrew Maguire <andrewm4894@gmail.com>
2023-07-16 17:46:01 +0100

    dev

--- ingest_jobs.py
+++ ingest_jobs.py
@@ -12,7 +12,7 @@ ingest_specs = {
         "dataset": "tmp", 
         "table": "test", 
         "project_id": os.getenv("GCP_PROJECT"),
-        "cron_schedule": "*/3 * * * *",
+        "cron_schedule": "*/2 * * * *",
     },   
     "m2": {
         "name": "m2", 
@@ -26,20 +26,24 @@ ingest_specs = {
 
 
 def build_ingest_job(spec) -> JobDefinition:
+    """Builds a job definition for a given ingest spec."""
     
     @job(name=spec["name"])
     def _job():
+        """Job definition for a given ingest spec."""
         
         logger = get_dagster_logger()
         
         @op(name=f"{spec['name']}_create_metrics")
         def create_metrics() -> pd.DataFrame:
+            """Creates metrics."""
             df = pd.read_gbq(query=spec['sql'])
             logger.info(f"df:\n{df}")
             return df
         
         @op(name=f"{spec['name']}_save_metrics")
         def save_metrics(df) -> pd.DataFrame:
+            """Saves metrics."""
             df.to_gbq(
                 destination_table=f"{spec['dataset']}.{spec['table']}",
                 project_id=spec['project_id'],
@@ -52,11 +56,13 @@ def build_ingest_job(spec) -> JobDefinition:
     return _job
 
 
+# generate jobs
 jobs = [
     build_ingest_job(ingest_specs[ingest_spec]) 
     for ingest_spec in ingest_specs 
 ]
 
+# define schedules
 schedules = [
     ScheduleDefinition(
         job=job,
@@ -65,6 +71,7 @@ schedules = [
     for job in jobs
 ]
 
+# create defs
 defs = Definitions(
     jobs=jobs,
     schedules=schedules,
