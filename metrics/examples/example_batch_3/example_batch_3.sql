c8560926a6f0572d98d3c76e1ff496eca236c941
Andrew Maguire <andrewm4894@gmail.com>
2023-08-01 21:10:46 +0100

    make sql example clearer

--- metrics/examples/example_batch_3/example_batch_3.sql
+++ metrics/examples/example_batch_3/example_batch_3.sql
@@ -1,15 +1,29 @@
+with
+
+metric_1 as
+(
 select
-  *
+  current_timestamp() as metric_timestamp,
+  'metric_1' as metric_name,
+  rand() as metric_value
+),
+
+metric_2 as
+(
+select
+  current_timestamp() as metric_timestamp,
+  'metric_2' as metric_name,
+  rand() as metric_value
+)
+
+select
+  metric_timestamp,
+  metric_name,
+  metric_value,
 from
   (
-    select
-      current_timestamp() as metric_timestamp,
-      'metric_1' as metric_name,
-      rand() as metric_value
-    union all
-    select
-      current_timestamp() as metric_timestamp,
-      'metric_2' as metric_name,
-      rand() as metric_value
+  select * from metric_1
+  union all
+  select * from metric_2
   )
 ;
