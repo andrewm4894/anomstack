94bbbdf3208168cb5d9ede54e349e4c80ecda57c
Andrew Maguire <andrewm4894@gmail.com>
2023-08-16 21:50:21 +0100

    add some docs

--- anomstack/README.md
+++ anomstack/README.md
@@ -1,3 +1,7 @@
 # anomstack
 
 Source code lives in here.
+
+- [`main.py`](./main.py) is the main entry point.
+- [`config.py`](./config.py) handles the configuration.
+- [`/jobs`](./jobs) contains the code for the various types of jobs (`ingest`, `train`, `score`, `alert`).
--- anomstack/config.py
+++ anomstack/config.py
@@ -6,16 +6,20 @@ import os
 import yaml
 from pathlib import Path
 
-env_vars = {
-    'ANOMSTACK_GCP_PROJECT_ID': 'gcp_project_id',
-    'ANOMSTACK_MODEL_PATH': 'model_path',
-    'ANOMSTACK_TABLE_KEY': 'table_key',
-}
+# environment variables that can be used to override the configuration.
+env_vars = [
+    'ANOMSTACK_GCP_PROJECT_ID',
+    'ANOMSTACK_MODEL_PATH',
+    'ANOMSTACK_TABLE_KEY'
+]
 
+# directories
 config_dir = Path('metrics')
 defaults_dir = Path('metrics/defaults')
 examples_dir = Path('metrics/examples')
+
 specs = {}
+
 
 def process_yaml_file(yaml_file):
     """
@@ -30,20 +34,27 @@ def process_yaml_file(yaml_file):
         
         if merged_specs['disable_batch'] == True:
             return None
-        for env_var, key in env_vars.items():
+        for env_var in env_vars:
             if env_var in os.environ:
-                merged_specs[key] = os.getenv(env_var)
+                merged_specs[env_var.replace('ANOMSTACK_','').lower()] = os.getenv(env_var)
 
         specs[metric_batch] = merged_specs
 
+
+# load defaults
 with open(defaults_dir / 'defaults.yaml', 'r') as file:    
     defaults = yaml.safe_load(file)
 
+# load all the YAML files
 for root, dirs, files in os.walk(config_dir):
+    
+    # ignore examples if the environment variable is set
     if os.getenv('ANOMSTACK_IGNORE_EXAMPLES') == 'yes' and examples_dir in Path(root).parents:        
         continue
     
+    # process all the YAML files
     for yaml_file in files:
+        
         if yaml_file == 'defaults.yaml':
             continue
         
