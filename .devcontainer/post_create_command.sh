f379ebcdd5954d826551a9f76579f142c1b98680
Andrew Maguire <andrewm4894@gmail.com>
2023-09-26 22:24:15 +0100

    devcontainer

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -2,10 +2,15 @@
 
 set -ex
 
+echo "start post create command"
+
+# show pwd
+echo "pwd: $(pwd)"
+
 # copy .env.example to .env
 cp .env.example .env
 
 # docker compose up
 docker compose up -d
 
-echo "done"
+echo "done post create command"
