d610ba17fff5d8760c24cd22f9c8a04b899ddd9c
Andrew Maguire <andrewm4894@gmail.com>
2023-10-13 21:42:46 +0100

    initial work to add snowflake

--- anomstack/df/save.py
+++ anomstack/df/save.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from anomstack.gcp.bigquery import save_df_bigquery
 from anomstack.utils.duckdb import save_df_duckdb
+from anomstack.snowflake.snowflake import save_df_snowflake
 
 
 def save_df(df, db, table_key, gcp_project_id, if_exists='append') -> pd.DataFrame:
@@ -13,9 +14,11 @@ def save_df(df, db, table_key, gcp_project_id, if_exists='append') -> pd.DataFra
 
     if db=='bigquery':
         df = save_df_bigquery(df, table_key, gcp_project_id, if_exists)
+    elif db=='snowflake':
+        df = save_df_snowflake(df, table_key)
     elif db=='duckdb':
         df = save_df_duckdb(df, table_key)
     else:
         raise ValueError(f'Unknown db: {db}')
-    
+
     return df
