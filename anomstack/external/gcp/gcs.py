d80df0446dbbae5151aefb09ca5b4cbc690d3dbb
Andrew Maguire <andrewm4894@gmail.com>
2023-10-20 20:20:12 +0100

    use anomstack gcp creds from env vars first if available

--- anomstack/external/gcp/gcs.py
+++ anomstack/external/gcp/gcs.py
@@ -3,37 +3,58 @@ from typing import List, Tuple
 from pyod.models.base import BaseDetector
 from dagster import get_dagster_logger
 import pickle
+import os
+from google.oauth2 import service_account
 
 
 def split_model_path(model_path) -> Tuple[str, str]:
     """
     Split model path into bucket and prefix.
     """
-    
+
     model_path_parts = model_path.split("://")
     model_path_bucket = model_path_parts[1].split("/")[0]
     model_path_prefix = "/".join(model_path_parts[1].split("/")[1:])
-    
+
     return model_path_bucket, model_path_prefix
+
+
+def get_storage_client():
+    """
+    Get GCS storage client with credentials.
+    """
+    creds_json = os.environ.get('ANOMSTACK_GOOGLE_APPLICATION_CREDENTIALS_JSON')
+    creds_file = os.environ.get('ANOMSTACK_GOOGLE_APPLICATION_CREDENTIALS')
+
+    if creds_file:
+        storage_client = storage.Client.from_service_account_json(creds_file)
+    elif creds_json:
+        creds = service_account.Credentials.from_authorized_user_info(creds_json)
+        storage_client = storage.Client(credentials=creds)
+    else:
+        storage_client = storage.Client()
+
+    return storage_client
 
 
 def save_models_gcs(models, model_path, metric_batch) -> List[Tuple[str, BaseDetector]]:
     """
     Save trained models to gcs bucket.
     """
-    
+
     logger = get_dagster_logger()
-    
+
     model_path_bucket, model_path_prefix = split_model_path(model_path)
-    
+
+    storage_client = get_storage_client()
+    bucket = storage_client.get_bucket(model_path_bucket)
+
     for metric, model in models:
-        
+
         model_name = f"{metric}.pkl"
         logger.info(f"saving {model_name} to {model_path}")
-        storage_client = storage.Client()
-        bucket = storage_client.get_bucket(model_path_bucket)
         blob = bucket.blob(f"{model_path_prefix}/{metric_batch}/{model_name}")
-        
+
         with blob.open("wb") as f:
             pickle.dump(model, f)
 
@@ -44,19 +65,20 @@ def load_model_gcs(metric_name, model_path, metric_batch) -> BaseDetector:
     """
     Load model.
     """
-    
+
     logger = get_dagster_logger()
-    
+
     model_path_bucket, model_path_prefix = split_model_path(model_path)
-    
+
     model_name = f'{metric_name}.pkl'
     logger.info(f'loading {model_name} from {model_path}')
-    storage_client = storage.Client()
+
+    storage_client = get_storage_client()
     bucket = storage_client.get_bucket(model_path_bucket)
     blob = bucket.blob(f'{model_path_prefix}/{metric_batch}/{model_name}')
-    
+
     with blob.open('rb') as f:
-        
+
         model = pickle.load(f)
-        
+
     return model
--- anomstack/ml/preprocess.py
+++ anomstack/ml/preprocess.py
@@ -5,18 +5,18 @@ from dagster import get_dagster_logger
 def make_x(df, mode='train', diff_n=0, smooth_n=0, lags_n=0, score_n=1) -> pd.DataFrame:
     """
     Prepare data for model training and scoring.
-    
+
     Parameters:
         diff_n (int): The order of differencing.
         smooth_n (int): The window size for smoothing (moving average).
         lags_n (list): The list of lags to include.
     """
-    
+
     logger = get_dagster_logger()
-    
+
     X = df.sort_values(by=['metric_timestamp']).reset_index(drop=True).set_index('metric_timestamp')
     X = df[["metric_value"]]
-    
+
     if diff_n > 0:
         X['metric_value'] = X['metric_value'].diff(periods=diff_n).dropna()
 
@@ -26,10 +26,10 @@ def make_x(df, mode='train', diff_n=0, smooth_n=0, lags_n=0, score_n=1) -> pd.Da
     if lags_n > 0:
         for lag in range(1,lags_n+1):
             X[f'lag_{lag}'] = X['metric_value'].shift(lag)
-    
+
     if mode == 'train':
         X = X.sample(frac=1)
-        
+
     elif mode == 'score':
         X = X.tail(score_n)
 
@@ -37,7 +37,7 @@ def make_x(df, mode='train', diff_n=0, smooth_n=0, lags_n=0, score_n=1) -> pd.Da
         raise ValueError(f"mode must be 'train' or 'score'")
 
     X = X.dropna()
-    
+
     logger.info(f'X=\n{X}')
-    
+
     return X
