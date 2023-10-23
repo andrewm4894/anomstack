2894d11979dbef603a896612b13916ee3898fda4
Andrew Maguire <andrewm4894@gmail.com>
2023-10-23 22:48:51 +0100

    add failure sensor

--- anomstack/main.py
+++ anomstack/main.py
@@ -2,20 +2,13 @@
 Main file for defining jobs and schedules.
 """
 
-import os
+from dagster import Definitions
 from anomstack.jobs.ingest import ingest_jobs, ingest_schedules
 from anomstack.jobs.train import train_jobs, train_schedules
 from anomstack.jobs.score import score_jobs, score_schedules
 from anomstack.jobs.alert import alert_jobs, alert_schedules
-from dagster import Definitions
-from dagster import make_email_on_run_failure_sensor
+from anomstack.sensors.failure import email_on_run_failure
 
-
-email_on_run_failure = make_email_on_run_failure_sensor(
-    email_from=os.getenv("ANOMSTACK_ALERT_EMAIL_FROM"),
-    email_password=os.getenv("ANOMSTACK_ALERT_EMAIL_PASSWORD"),
-    email_to=os.getenv("ANOMSTACK_ALERT_EMAIL_TO").split(","),
-)
 
 jobs = ingest_jobs + train_jobs + score_jobs + alert_jobs
 sensors = [email_on_run_failure]
---
+++ anomstack/sensors/failure.py
@@ -0,0 +1,19 @@
+import os
+from dagster import make_email_on_run_failure_sensor
+
+
+email_from = os.getenv(
+    "ANOMSTACK_FAILURE_EMAIL_FROM", os.getenv("ANOMSTACK_ALERT_EMAIL_FROM")
+)
+email_password = os.getenv(
+    "ANOMSTACK_FAILURE_EMAIL_PASSWORD", os.getenv("ANOMSTACK_ALERT_EMAIL_PASSWORD")
+)
+email_to = os.getenv(
+    "ANOMSTACK_FAILURE_EMAIL_TO", os.getenv("ANOMSTACK_ALERT_EMAIL_TO")
+).split(",")
+
+email_on_run_failure = make_email_on_run_failure_sensor(
+    email_from=email_from,
+    email_password=email_password,
+    email_to=email_to,
+)
