6c996f665e11c0d61b272ff7aeacaa951e0c4548
Andrew Maguire <andrewm4894@gmail.com>
2023-10-28 22:45:04 +0100

    handle openai rate limiting

--- anomstack/llm/completion.py
+++ anomstack/llm/completion.py
@@ -1,13 +1,15 @@
 import json
 import openai
+import time
 
 
-def get_completion(prompt: str, model="gpt-4-0613"):
+def get_completion(prompt: str, model="gpt-4-0613", max_retries=5):
     """
     Get a completion from the OpenAI API.
 
     Args:
         prompt (str): The prompt to send to the OpenAI API.
+        max_retries (int): The maximum number of retries before giving up.
 
     Returns:
         Tuple[bool, str]: A tuple containing a boolean indicating whether the metric looks anomalous,
@@ -16,36 +18,48 @@ def get_completion(prompt: str, model="gpt-4-0613"):
 
     messages = [{"role": "user", "content": prompt}]
 
-    completion = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        functions=[
-            {
-                "name": "trigger_anomaly_alert",
-                "description": "If the metric looks anomalous then flag it as anomalous.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "is_anomalous": {
-                            "type": "boolean",
-                            "description": "True if the metric looks anomalous, False otherwise.",
+    retries = 0
+    while retries < max_retries:
+        try:
+            completion = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                functions=[
+                    {
+                        "name": "trigger_anomaly_alert",
+                        "description": "If the metric looks anomalous then flag it as anomalous.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "is_anomalous": {
+                                    "type": "boolean",
+                                    "description": "True if the metric looks anomalous, False otherwise.",
+                                },
+                                "anomaly_description": {
+                                    "type": "string",
+                                    "description": "A description of the anomaly, if is_anomalous=True, else None.",
+                                },
+                            },
+                            "required": ["is_anomalous", "anomaly_description"],
                         },
-                        "anomaly_description": {
-                            "type": "string",
-                            "description": "A description of the anomaly, if is_anomalous=True, else None.",
-                        },
-                    },
-                    "required": ["is_anomalous", "anomaly_description"],
-                },
-            }
-        ],
-        function_call={"name": "trigger_anomaly_alert"},
-    )
+                    }
+                ],
+                function_call={"name": "trigger_anomaly_alert"},
+            )
+            break
+        except openai.error.RateLimitError as e:
+            print(f"Rate limit reached. Waiting for {e.retry_after} seconds...")
+            time.sleep(e.retry_after)
+            retries += 1
+
+    if retries == max_retries:
+        raise ValueError("Maximum number of retries reached. Aborting.")
+
     reply_content = completion.choices[0]
 
-    funcs = reply_content['message'].to_dict()['function_call']['arguments']
+    funcs = reply_content["message"].to_dict()["function_call"]["arguments"]
     funcs = json.loads(funcs)
-    is_anomalous = funcs['is_anomalous']
-    anomaly_description = funcs['anomaly_description']
+    is_anomalous = funcs["is_anomalous"]
+    anomaly_description = funcs["anomaly_description"]
 
     return is_anomalous, anomaly_description
