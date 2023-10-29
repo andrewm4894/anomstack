b8619b7ce2ac4445b71de7c7128dc812f4771e1e
Andrew Maguire <andrewm4894@gmail.com>
2023-10-29 21:57:13 +0000

    try fix headers

--- metrics/examples/yfinance/yfinance.py
+++ metrics/examples/yfinance/yfinance.py
@@ -8,8 +8,12 @@ def ingest() -> pd.DataFrame:
     import time
     import pandas as pd
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+    headers = { 
+        'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
+        'Accept'          : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+        'Accept-Language' : 'en-US,en;q=0.5',
+        'DNT'             : '1', # Do Not Track Request Header 
+        'Connection'      : 'close'
     }
 
     tickers = ["AAPL", "MSFT", "GOOG", "NVDA"]
