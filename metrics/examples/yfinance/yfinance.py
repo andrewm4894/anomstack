5f85e83d72503ce1723c90aea244021137653ac4
Andrew Maguire <andrewm4894@gmail.com>
2023-10-29 01:04:29 +0100

    try different user agent

--- metrics/examples/yfinance/yfinance.py
+++ metrics/examples/yfinance/yfinance.py
@@ -7,7 +7,7 @@ def ingest() -> pd.DataFrame:
     import requests_cache
 
     session = requests_cache.CachedSession('yfinance.cache')
-    session.headers['User-agent'] = 'Dagster Anomstack'
+    session.headers['User-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
 
     prices = []
     tickers = ['AAPL','MSFT','GOOG','NVDA']
