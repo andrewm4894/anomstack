f79146668ee1b3e8114d7187032ddd2b66af5ea7
Andrew Maguire <andrewm4894@gmail.com>
2023-10-29 21:45:46 +0000

    simplify yfinance example

--- metrics/examples/yfinance/yfinance.py
+++ metrics/examples/yfinance/yfinance.py
@@ -1,25 +1,38 @@
-def ingest() -> pd.DataFrame:
-    """
-    """
+import pandas as pd
 
+
+def ingest() -> pd.DataFrame:
+    """ """
+
+    import requests
+    import time
     import pandas as pd
-    import yfinance as yf
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+    }
+
+    tickers = ["AAPL", "MSFT", "GOOG", "NVDA"]
 
     prices = []
-    tickers = ['AAPL','MSFT','GOOG','NVDA']
+    metric_names = []
+
     for ticker in tickers:
-        stock = yf.Ticker(
-            ticker
-        )
-        latest_price = stock.info["currentPrice"]
-        prices.append(latest_price)
+        time.sleep(1)
+        url = f"https://query2.finance.yahoo.com/v6/finance/quoteSummary/{ticker}?modules=financialData&ssl=true"
+        response = requests.get(url, headers=headers, timeout=10)
+        if response.status_code == 200:
+            data = response.json()
+            price = data["quoteSummary"]["result"][0]["financialData"]["currentPrice"]["raw"]
+            prices.append(price)
+            metric_names.append(f"yf_{ticker.lower()}_price")
 
     df = pd.DataFrame(
         {
-            "metric_name": [f'yf_{t.lower()}_price' for t in tickers],
+            "metric_name": metric_names,
             "metric_value": prices,
         }
     )
-    df['metric_timestamp'] = pd.Timestamp.utcnow()
+    df["metric_timestamp"] = pd.Timestamp.utcnow()
 
     return df
