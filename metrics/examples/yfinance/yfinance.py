b248e86b15f4294c01584c1752b68c2daa2f2c69
Andrew Maguire <andrewm4894@gmail.com>
2023-10-29 22:50:38 +0000

    fix yfinance

--- metrics/examples/yfinance/yfinance.py
+++ metrics/examples/yfinance/yfinance.py
@@ -2,34 +2,52 @@ import pandas as pd
 
 
 def ingest() -> pd.DataFrame:
-    """ """
+    """
+    Credit to: https://stackoverflow.com/a/76580610/1919374 
+    """
 
     import requests
-    import time
     import pandas as pd
 
-    headers = { 
-        'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
-        'Accept'          : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
-        'Accept-Language' : 'en-US,en;q=0.5',
-        'DNT'             : '1', # Do Not Track Request Header 
-        'Connection'      : 'close'
-    }
+    
+    apiBase = "https://query2.finance.yahoo.com"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64)"}
+    
+    
+    def getCredentials(
+        cookieUrl="https://fc.yahoo.com", crumbUrl=apiBase + "/v1/test/getcrumb"
+    ):
+        cookie = requests.get(cookieUrl, timeout=10).cookies
+        crumb = requests.get(
+            url=crumbUrl, cookies=cookie, headers=headers, timeout=10
+        ).text
+        return {"cookie": cookie, "crumb": crumb}
 
-    tickers = ["AAPL", "MSFT", "GOOG", "NVDA"]
+    def quote(symbols, credentials):
+        url = apiBase + "/v7/finance/quote"
+        params = {"symbols": ",".join(symbols), "crumb": credentials["crumb"]}
+        response = requests.get(
+            url,
+            params=params,
+            cookies=credentials["cookie"],
+            headers=headers,
+            timeout=10,
+        )
+        quotes = response.json()["quoteResponse"]["result"]
+        return quotes
 
-    prices = []
-    metric_names = []
 
-    for ticker in tickers:
-        time.sleep(1)
-        url = f"https://query2.finance.yahoo.com/v6/finance/quoteSummary/{ticker}?modules=financialData&ssl=true"
-        response = requests.get(url, headers=headers, timeout=10)
-        if response.status_code == 200:
-            data = response.json()
-            price = data["quoteSummary"]["result"][0]["financialData"]["currentPrice"]["raw"]
-            prices.append(price)
-            metric_names.append(f"yf_{ticker.lower()}_price")
+    symbols = ["GOOG", "TSLA", "AAPL", "MSFT"]
+
+    credentials = getCredentials()
+    quotes = quote(symbols, credentials)
+
+    if quotes:
+        prices = []
+        metric_names = []
+        for quote in quotes:
+            prices.append(quote["regularMarketPrice"])
+            metric_names.append(f"yf_{quote['symbol'].lower()}_price")
 
     df = pd.DataFrame(
         {
