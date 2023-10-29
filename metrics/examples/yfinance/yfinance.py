4139dd16739d446ef34fd035d7a8429fa3746ade
Andrew Maguire <andrewm4894@gmail.com>
2023-10-29 01:26:58 +0100

    remove requests cache

--- metrics/examples/yfinance/yfinance.py
+++ metrics/examples/yfinance/yfinance.py
@@ -4,15 +4,13 @@ def ingest() -> pd.DataFrame:
 
     import pandas as pd
     import yfinance as yf
-    import requests_cache
-
-    session = requests_cache.CachedSession('yfinance.cache')
-    session.headers['User-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
 
     prices = []
     tickers = ['AAPL','MSFT','GOOG','NVDA']
     for ticker in tickers:
-        stock = yf.Ticker(ticker, session=session)
+        stock = yf.Ticker(
+            ticker
+        )
         latest_price = stock.info["currentPrice"]
         prices.append(latest_price)
 
