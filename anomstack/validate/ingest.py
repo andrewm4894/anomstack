c767d669b784610fd0c96daaf59250ced89069ac
Andrew Maguire <andrewm4894@gmail.com>
2023-11-03 13:11:33 +0000

    check should not case about case

--- anomstack/validate/ingest.py
+++ anomstack/validate/ingest.py
@@ -7,9 +7,9 @@ def validate_ingest_df(df: pd.DataFrame) -> pd.DataFrame:
     """
 
     # validate the dataframe
-    assert 'metric_name' in df.columns, 'metric_name column missing'
-    assert 'metric_value' in df.columns, 'metric_value column missing'
-    assert 'metric_timestamp' in df.columns, 'metric_timestamp column missing'
+    assert 'metric_name' in df.columns.str.lower(), 'metric_name column missing'
+    assert 'metric_value' in df.columns.str.lower(), 'metric_value column missing'
+    assert 'metric_timestamp' in df.columns.str.lower(), 'metric_timestamp column missing'
     assert len(df.columns) == 3, 'too many columns'
     assert len(df) > 0, 'no data returned'
 
