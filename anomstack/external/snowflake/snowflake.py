2359cd48bda86bde7aaa50703684208fc722cabc
Andrew Maguire <andrewm4894@gmail.com>
2023-11-07 13:45:16 +0000

    Fix snowflake connector error by converting metric timestamp to string

--- anomstack/external/snowflake/snowflake.py
+++ anomstack/external/snowflake/snowflake.py
@@ -42,6 +42,8 @@ def save_df_snowflake(df, table_key, cols_lowercase=True) -> pd.DataFrame:
     Save df to db.
     """
 
+    logger = get_dagster_logger()
+
     table_key_parts = table_key.split('.')
 
     credentials = get_snowflake_credentials()
@@ -52,7 +54,13 @@ def save_df_snowflake(df, table_key, cols_lowercase=True) -> pd.DataFrame:
         password=credentials['snowflake_password'],
         warehouse=credentials['snowflake_warehouse'],
     )
-    success, nchunks, nrows, _ = write_pandas(
+
+    # convert metric timestamp to string
+    # fixes: nowflake.connector.errors.ProgrammingError: 002023 (22000): SQL compilation error: Expression type does not match column data type, expecting TIMESTAMP_NTZ(9) but got NUMBER(38,0) for column METRIC_TIMESTAMP
+    # TODO: why do i have to do this?
+    df['metric_timestamp'] = df['metric_timestamp'].astype(str)
+
+    success, nchunks, nrows, output = write_pandas(
         conn,
         df,
         database=table_key_parts[0],
