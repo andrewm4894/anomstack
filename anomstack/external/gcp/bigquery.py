869c4766ef5a282611fcd7686931af925e13a818
Andrew Maguire <andrewm4894@gmail.com>
2023-11-07 14:11:22 +0000

    Add imports and implement exponential backoff retry for saving data to BigQuery

--- anomstack/external/gcp/bigquery.py
+++ anomstack/external/gcp/bigquery.py
@@ -5,6 +5,11 @@ from dagster import get_dagster_logger
 import pandas as pd
 from anomstack.external.gcp.credentials import get_google_credentials
 import os
+import time
+import random
+from google.api_core.exceptions import Forbidden
+from google.cloud import bigquery
+from google.cloud.exceptions import TooManyRequests
 
 
 def read_sql_bigquery(sql) -> pd.DataFrame:
@@ -14,7 +19,7 @@ def read_sql_bigquery(sql) -> pd.DataFrame:
 
     logger = get_dagster_logger()
 
-    logger.debug(f'sql:\n{sql}')
+    logger.debug(f"sql:\n{sql}")
 
     credentials = get_google_credentials()
 
@@ -22,24 +27,28 @@ def read_sql_bigquery(sql) -> pd.DataFrame:
         query=sql,
         credentials=credentials,
     )
-    logger.debug(f'df:\n{df}')
+    logger.debug(f"df:\n{df}")
 
     return df
 
 
-def save_df_bigquery(df, table_key, if_exists='append') -> pd.DataFrame:
+def pandas_save_df_bigquery(df, table_key, if_exists="append") -> pd.DataFrame:
     """
     Save df to db.
     """
 
-    table_key_parts = table_key.split('.')
+    table_key_parts = table_key.split(".")
 
     if len(table_key_parts) == 2:
-        project_id = os.getenv('ANOMSTACK_GCP_PROJECT_ID')
-        assert project_id is not None, f'ANOMSTACK_GCP_PROJECT_ID must be set in environment if table_key is not fully qualified: {table_key}'
+        project_id = os.getenv("ANOMSTACK_GCP_PROJECT_ID")
+        assert (
+            project_id is not None
+        ), f"ANOMSTACK_GCP_PROJECT_ID must be set in environment if table_key is not fully qualified: {table_key}"
         table_key_parts = [project_id] + table_key_parts
 
-    assert len(table_key_parts) == 3, f'Invalid table_key: {table_key}, should be <project_id>.<dataset_id>.<table_id>'
+    assert (
+        len(table_key_parts) == 3
+    ), f"Invalid table_key: {table_key}, should be <project_id>.<dataset_id>.<table_id>"
 
     project_id = table_key_parts[0]
     dataset_id = table_key_parts[1]
@@ -48,10 +57,64 @@ def save_df_bigquery(df, table_key, if_exists='append') -> pd.DataFrame:
     credentials = get_google_credentials()
 
     df.to_gbq(
-        destination_table=f'{dataset_id}.{table_id}',
+        destination_table=f"{dataset_id}.{table_id}",
         project_id=project_id,
         if_exists=if_exists,
         credentials=credentials,
     )
+
+    return df
+
+
+def save_df_bigquery(df, table_key, if_exists="append", max_retries=5) -> pd.DataFrame:
+    """
+    Save df to db, with exponential backoff retry for handling rate limit exceeded error.
+    """
+    table_key_parts = table_key.split(".")
+    if len(table_key_parts) == 2:
+        project_id = os.getenv("ANOMSTACK_GCP_PROJECT_ID")
+        assert (
+            project_id is not None
+        ), "ANOMSTACK_GCP_PROJECT_ID must be set in environment if table_key is not fully qualified."
+        table_key_parts = [project_id] + table_key_parts
+
+    assert (
+        len(table_key_parts) == 3
+    ), "Invalid table_key, should be <project_id>.<dataset_id>.<table_id>"
+
+    project_id, dataset_id, table_id = table_key_parts
+
+    credentials = get_google_credentials()
+
+    client = bigquery.Client(credentials=credentials, project=project_id)
+
+    destination_table = f"{dataset_id}.{table_id}"
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.job.WriteDisposition.WRITE_APPEND
+        if if_exists == "append"
+        else bigquery.job.WriteDisposition.WRITE_TRUNCATE
+    )
+
+    for attempt in range(max_retries):
+        try:
+            job = client.load_table_from_dataframe(
+                dataframe=df,
+                destination=destination_table,
+                job_config=job_config
+            )
+            job.result()  # Wait for the job to complete
+            break  # Success, exit the retry loop
+        except (TooManyRequests, Forbidden) as e:
+            wait_time = 2**attempt + random.uniform(
+                0, 1
+            )  # Exponential backoff with jitter
+            get_dagster_logger().warning(
+                f"Exceeded rate limits on attempt {attempt+1}. Retrying in {wait_time} seconds."
+            )
+            time.sleep(wait_time)
+    else:
+        raise RuntimeError(
+            f"Failed to save data to BigQuery after {max_retries} attempts."
+        )
 
     return df
