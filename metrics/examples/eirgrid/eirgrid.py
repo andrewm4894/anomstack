46232fb6ebb210c33d76a413d31a57ca9f13904f
Andrew Maguire <andrewm4894@gmail.com>
2023-11-28 22:48:13 +0000

    add additional metrics

--- metrics/examples/eirgrid/eirgrid.py
+++ metrics/examples/eirgrid/eirgrid.py
@@ -10,27 +10,45 @@ def ingest() -> pd.DataFrame:
 
     import requests
 
+    def make_df(data) -> pd.DataFrame:
+
+        df = pd.DataFrame(data['Rows'])
+        df = df[['EffectiveTime', 'FieldName', 'Value']]
+        df.columns = ['metric_timestamp', 'metric_name', 'metric_value']
+        df = df.dropna()
+        df['metric_timestamp'] = pd.to_datetime(df['metric_timestamp'])
+        df = df.sort_values(by='metric_timestamp', ascending=False).head(1)
+        df['metric_name'] = df['metric_name'].str.lower()
+        df['metric_name'] = 'eirgrid_' + df['metric_name']
+        df = df[["metric_timestamp", "metric_name", "metric_value"]]
+        
+        return df
+
     today = date.today().strftime("%d-%b-%Y")
 
-    url = f'https://www.smartgriddashboard.com/DashboardService.svc/data?area=demandactual&region=ALL&datefrom={today}+00%3A00&dateto={today}+23%3A59'
+    area_list = [
+        'demandactual',
+        'demandforecast',
+        'generationactual',
+        'windactual',
+        'windforecast',
+        'frequency'
+    ]
+
+    urls = [
+        f'https://www.smartgriddashboard.com/DashboardService.svc/data?area={area}&region=ALL&datefrom={today}+00%3A00&dateto={today}+23%3A59'
+        for area in area_list
+    ]
 
     headers = {}
 
-    response = requests.get(url, headers=headers)
+    df = pd.DataFrame()
 
-    data = response.json()
-
-    df = pd.DataFrame(data['Rows'])
-    df = df[df['Region'] == 'ALL']
-    df = df[df['FieldName'] == 'SYSTEM_DEMAND']
-    df = df[['EffectiveTime', 'FieldName', 'Value']]
-    df.columns = ['metric_timestamp', 'metric_name', 'metric_value']
-    df = df.dropna()
-    df['metric_timestamp'] = pd.to_datetime(df['metric_timestamp'])
-    df = df.sort_values(by='metric_timestamp', ascending=False).head(1)
-    df['metric_name'] = df['metric_name'].str.lower()
-    df['metric_name'] = 'eirgrid_' + df['metric_name']
-    df = df[["metric_timestamp", "metric_name", "metric_value"]]
+    for url in urls:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        df_metric = make_df(data)
+        df = pd.concat([df, df_metric])
 
     return df
 
