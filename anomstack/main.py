e60ef3a00182af41e055387c2b730b275c116bef
Andrew Maguire <andrewm4894@gmail.com>
2023-12-07 15:00:42 +0000

    comment out failure sensors until they are fixed

--- anomstack/main.py
+++ anomstack/main.py
@@ -10,10 +10,10 @@ from anomstack.jobs.llmalert import llmalert_jobs, llmalert_schedules
 from anomstack.jobs.plot import plot_jobs, plot_schedules
 from anomstack.jobs.score import score_jobs, score_schedules
 from anomstack.jobs.train import train_jobs, train_schedules
-from anomstack.sensors.failure import email_on_run_failure
+# from anomstack.sensors.failure import email_on_run_failure
 
 jobs = ingest_jobs + train_jobs + score_jobs + alert_jobs + llmalert_jobs + plot_jobs
-sensors = [email_on_run_failure]
+# sensors = [email_on_run_failure]
 schedules = (
     ingest_schedules
     + train_schedules
@@ -26,5 +26,5 @@ schedules = (
 defs = Definitions(
     jobs=jobs,
     schedules=schedules,
-    sensors=sensors,
+    # sensors=sensors,
 )
