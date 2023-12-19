e33e591131a81df9a51824d0284c9cf4c4ff503b
Andrew Maguire <andrewm4894@gmail.com>
2023-12-19 16:44:31 +0000

    handle if env var in none

--- anomstack/sensors/failure.py
+++ anomstack/sensors/failure.py
@@ -1,9 +1,6 @@
 import os
 
 from dagster import make_email_on_run_failure_sensor
-from dotenv import load_dotenv
-
-load_dotenv()
 
 email_from = os.getenv(
     "ANOMSTACK_FAILURE_EMAIL_FROM", os.getenv("ANOMSTACK_ALERT_EMAIL_FROM")
@@ -12,7 +9,7 @@ email_password = os.getenv(
     "ANOMSTACK_FAILURE_EMAIL_PASSWORD", os.getenv("ANOMSTACK_ALERT_EMAIL_PASSWORD")
 )
 email_to = os.getenv(
-    "ANOMSTACK_FAILURE_EMAIL_TO", os.getenv("ANOMSTACK_ALERT_EMAIL_TO")
+    "ANOMSTACK_FAILURE_EMAIL_TO", os.getenv("ANOMSTACK_ALERT_EMAIL_TO", "")
 ).split(",")
 
 email_on_run_failure = make_email_on_run_failure_sensor(
