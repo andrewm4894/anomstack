40a0819279bbc347d66a1e75ce262ba395656ab7
Andrew Maguire <andrewm4894@gmail.com>
2024-02-02 23:53:40 +0000

    Add send_df function to send alerts with a DataFrame
    This commit adds a new function called `send_df` to the `send.py` file in the `anomstack.alerts` module. The `send_df` function allows sending alerts with a DataFrame using the specified alert methods, such as email and Slack. The function takes a title, a DataFrame, and optional parameters like alert methods and description. It converts the DataFrame to HTML and sends the alert using the specified methods.

--- anomstack/alerts/send.py
+++ anomstack/alerts/send.py
@@ -6,7 +6,7 @@ import pandas as pd
 from dagster import get_dagster_logger
 
 from anomstack.alerts.asciiart import make_alert_message
-from anomstack.alerts.email import send_email_with_plot
+from anomstack.alerts.email import send_email_with_plot, send_email
 from anomstack.alerts.slack import send_alert_slack
 
 
@@ -52,6 +52,39 @@ def send_alert(
             attachment_name=metric_name,
             threshold=threshold,
             score_col=score_col,
+        )
+
+    return df
+
+
+def send_df(
+    title: str,
+    df: pd.DataFrame,
+    alert_methods: str = "email,slack",
+    description: str = "",
+) -> pd.DataFrame:
+    """
+    Sends a df using the specified alert methods.
+
+    Args:
+        title (str): The title of the alert.
+        df (pd.DataFrame): The data to be included in the alert.
+        alert_methods (str, optional): The alert methods to use, separated by commas. Defaults to 'email,slack'.
+        description (str, optional): The description of the alert. Defaults to ''.
+        tags (list, optional): The tags to be included in the alert. Defaults to None.
+
+    Returns:
+        pd.DataFrame: The input DataFrame.
+    """
+    logger = get_dagster_logger()
+    logger.debug(f"alerts to send: \n{df}")
+    message = df.to_html()
+    if "slack" in alert_methods:
+        send_alert_slack(title=title, message=message)
+    if "email" in alert_methods:
+        send_email(
+            subject=title,
+            body=message,
         )
 
     return df
