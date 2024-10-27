203c531cc1e4fb28ccd6618ad1d6fdca832de145
andrewm4894 <andrewm4894@gmail.com>
2024-10-27 18:12:29 +0000

    Refactor send_alert_slack function in slack.py

--- anomstack/alerts/slack.py
+++ anomstack/alerts/slack.py
@@ -1,13 +1,51 @@
 """
 Helper functions for sending alerts via Slack.
+
+Slack alerts use a slack app. Below is a copy of the app manifest so you can
+create your own. Instructions for creating a slack app from a manifest can
+be found here: https://api.slack.com/reference/manifests#creating_apps
+
+```json
+{
+    "display_information": {
+        "name": "anomstack"
+    },
+    "features": {
+        "bot_user": {
+            "display_name": "anomstack",
+            "always_online": false
+        }
+    },
+    "oauth_config": {
+        "redirect_urls": [
+            "https://anomstack.com/"
+        ],
+        "scopes": {
+            "bot": [
+                "channels:read",
+                "chat:write",
+                "files:write",
+                "groups:read"
+            ]
+        }
+    },
+    "settings": {
+        "org_deploy_enabled": false,
+        "socket_mode_enabled": false,
+        "token_rotation_enabled": false
+    }
+}
+```
 """
 
 import os
+import tempfile
+
+import matplotlib.pyplot as plt
+from dagster import get_dagster_logger
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from dagster import get_dagster_logger
-import matplotlib.pyplot as plt
-import tempfile
+
 from anomstack.plots.plot import make_alert_plot
 
 
@@ -15,7 +53,6 @@ def send_alert_slack(
     title="alert",
     message="hello",
     image_file_path=None,
-    env_var_bot_token="ANOMSTACK_SLACK_BOT_TOKEN",
     channel_name=None
 ) -> None:
     """
@@ -24,11 +61,13 @@ def send_alert_slack(
     Args:
         title (str, optional): Title of the alert. Defaults to "alert".
         message (str, optional): Message of the alert. Defaults to "hello".
-        image_file_path (str, optional): Path to image file to upload. Defaults to None.
-        env_var_bot_token (str, optional): Environment variable name for the bot token.
-            Defaults to "ANOMSTACK_SLACK_BOT_TOKEN".
-        channel_name (str, optional): Slack channel name to send the message to.
-            Defaults to None, in which case the environment variable 'ANOMSTACK_SLACK_CHANNEL' is used.
+        image_file_path (str, optional): Path to image file to upload.
+            Defaults to None.
+        env_var_bot_token (str, optional): Environment variable name for
+            the bot token. Defaults to "ANOMSTACK_SLACK_BOT_TOKEN".
+        channel_name (str, optional): Slack channel name to send the message
+            to. Defaults to None, in which case the environment variable
+            'ANOMSTACK_SLACK_CHANNEL' is used.
 
     Returns:
         None
@@ -36,9 +75,14 @@ def send_alert_slack(
 
     logger = get_dagster_logger()
 
-    slack_token = os.environ.get(env_var_bot_token)
+    slack_token = os.environ.get('ANOMSTACK_SLACK_BOT_TOKEN')
     if not slack_token:
-        raise ValueError(f"Slack bot token not found in environment variable {env_var_bot_token}")
+        raise ValueError(
+            (
+                "Slack bot token not found in environment variable "
+                "ANOMSTACK_SLACK_BOT_TOKEN"
+            )
+        )
 
     client = WebClient(token=slack_token)
 
@@ -46,16 +90,19 @@ def send_alert_slack(
         channel_name = os.environ.get('ANOMSTACK_SLACK_CHANNEL')
         if not channel_name:
             raise ValueError(
-                "Slack channel not specified in environment variable 'ANOMSTACK_SLACK_CHANNEL' or parameter channel_name"
+                (
+                    "Slack channel not specified in environment variable "
+                    "'ANOMSTACK_SLACK_CHANNEL' or parameter channel_name"
+                )
             )
 
-    # Remove leading '#' if present
     if channel_name.startswith('#'):
         channel_name = channel_name[1:]
 
-    # Get the channel ID
     try:
-        response = client.conversations_list(types='public_channel,private_channel')
+        response = client.conversations_list(
+            types='public_channel,private_channel'
+        )
         channels = response['channels']
         channel_id = None
         for channel in channels:
@@ -70,9 +117,7 @@ def send_alert_slack(
         return
 
     try:
-        # Send message with or without image
         if image_file_path:
-            # Upload the image
             with open(image_file_path, 'rb') as file_content:
                 response = client.files_upload_v2(
                     channels=[channel_id],
@@ -81,7 +126,6 @@ def send_alert_slack(
                     filename=os.path.basename(image_file_path)
                 )
         else:
-            # Send message without image
             response = client.chat_postMessage(
                 channel=channel_id,
                 text=f"*{title}*\n{message}"
@@ -98,29 +142,32 @@ def send_alert_slack_with_plot(
     message,
     threshold=0.8,
     score_col="metric_score_smooth",
-    env_var_bot_token="ANOMSTACK_SLACK_BOT_TOKEN",
     channel_name=None
 ) -> None:
     """
     Sends an alert to Slack with a plot attached.
     """
-    logger = get_dagster_logger()
 
-    # Generate the plot and save to temporary file
-    with tempfile.NamedTemporaryFile(prefix=metric_name, suffix=".png", delete=False) as temp:
-        fig = make_alert_plot(df, metric_name, threshold, score_col)
+    if not channel_name:
+        channel_name = os.environ.get('ANOMSTACK_SLACK_CHANNEL')
+
+    with tempfile.NamedTemporaryFile(
+        prefix=metric_name, suffix=".png",
+        delete=False
+    ) as temp:
+        fig = make_alert_plot(
+            df, metric_name, threshold,
+            score_col
+        )
         fig.savefig(temp.name)
-        plt.close(fig)  # Properly close the figure
+        plt.close(fig)
 
-        # Send the alert to Slack
         try:
             send_alert_slack(
                 title=title,
                 message=message,
                 image_file_path=temp.name,
-                env_var_bot_token=env_var_bot_token,
                 channel_name=channel_name
             )
         finally:
-            # Clean up the temp file
             os.unlink(temp.name)
