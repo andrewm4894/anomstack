d05487af0978b4ef468d980b67f89a0e0f62d576
andrewm4894 <andrewm4894@gmail.com>
2024-11-16 00:25:30 +0000

    Add metric score and alert calculations to llmalert SQL template

--- metrics/defaults/sql/llmalert.sql
+++ metrics/defaults/sql/llmalert.sql
@@ -22,6 +22,42 @@ where
 group by metric_timestamp, metric_batch, metric_name
 ),
 
+metric_score_data as 
+(
+select distinct
+  metric_timestamp,
+  metric_batch,
+  metric_name,
+  avg(metric_value) AS metric_score
+from 
+  {{ table_key }}
+where
+  metric_batch = '{{ metric_batch }}'
+  and 
+  metric_type = 'score'
+  and 
+  date(metric_timestamp) >= date('now', '-{{ llmalert_metric_timestamp_max_days_ago }} day')
+group by metric_timestamp, metric_batch, metric_name
+),
+
+metric_alert_data as 
+(
+select distinct
+  metric_timestamp,
+  metric_batch,
+  metric_name,
+  avg(metric_value) AS metric_alert
+from 
+  {{ table_key }}
+where
+  metric_batch = '{{ metric_batch }}'
+  and 
+  metric_type = 'alert'
+  and 
+  date(metric_timestamp) >= date('now', '-{{ llmalert_metric_timestamp_max_days_ago }} day')
+group by metric_timestamp, metric_batch, metric_name
+),
+
 metric_value_recency_ranked as 
 (
 select
@@ -35,10 +71,28 @@ from
 )
 
 select
-  metric_timestamp,
-  metric_batch,
-  metric_name,
-  metric_value
+  m.metric_timestamp,
+  m.metric_batch,
+  m.metric_name,
+  m.metric_value,
+  ifnull(s.metric_score,0) as metric_score,
+  ifnull(a.metric_alert,0) as metric_alert
 from 
-  metric_value_recency_ranked
+  metric_value_recency_ranked m
+left join
+  metric_score_data s
+on
+  m.metric_timestamp = s.metric_timestamp
+  and
+  m.metric_batch = s.metric_batch
+  and
+  m.metric_name = s.metric_name
+left join
+  metric_alert_data a
+on
+  m.metric_timestamp = a.metric_timestamp
+  and
+  m.metric_batch = a.metric_batch
+  and
+  m.metric_name = a.metric_name
 ;
