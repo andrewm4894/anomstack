33e69aefcc59fd5a2cfd635d2fac1b200a62a309
Andrew Maguire <andrewm4894@gmail.com>
2024-12-13 22:44:05 +0000

    Add .gitignore and update main.py to load environment variables and display metric batches

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,8 +1,16 @@
-from fasthtml.common import *
+from dotenv import load_dotenv
+from fasthtml.common import Div, P, fast_app, serve
+
+from anomstack.config import specs
+
+load_dotenv('./.env')
 
 app,rt = fast_app()
 
 @rt('/')
-def get(): return Div(P('Hello World!'), hx_get="/change")
+def get():
+    metric_batches = list(specs.keys())
+    return Div(P(metric_batches))
+
 
 serve()
