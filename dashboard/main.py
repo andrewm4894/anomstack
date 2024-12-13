025b564d3678d97a1919c47c2c0094bd8e8401f7
Andrew Maguire <andrewm4894@gmail.com>
2024-12-13 23:07:21 +0000

    Add SQL data retrieval to dashboard endpoint

--- dashboard/main.py
+++ dashboard/main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from fasthtml.common import Div, P, fast_app, serve
 
 from anomstack.config import specs
+from anomstack.jinja.render import render
+from anomstack.sql.read import read_sql
 
 load_dotenv('./.env')
 
@@ -10,6 +12,17 @@ app,rt = fast_app()
 @rt('/')
 def get():
     metric_batches = list(specs.keys())
+
+    for smetric_batch in specs:
+
+        spec = specs[smetric_batch]
+
+        # get data
+        sql = render("dashboard_sql", spec, params={"alert_max_n": 90})
+        db = spec["db"]
+        df = read_sql(sql, db=db)
+        print(df.head())
+
     return Div(P(metric_batches))
 
 
