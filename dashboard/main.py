2eec3380d801ae8a935b4f2bc58bb458eb5c5308
andrewm4894 <andrewm4894@gmail.com>
2024-12-14 18:20:59 +0000

    Refactor main.py to simplify environment variable handling and improve logging; update home route to return title and figures

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,43 +1,22 @@
 import logging
-import os
-
-from dotenv import load_dotenv
-from fasthtml.common import Div, P, Table, fast_app, serve
+from fasthtml.common import Div, P, Table, fast_app, serve, Title
 from utils import get_enabled_dagster_jobs
 
 from anomstack.config import specs
 from anomstack.jinja.render import render
 from anomstack.sql.read import read_sql
 
-load_dotenv('./.env')
 
-
-# Resolve DAGSTER_HOME to an absolute path
-dagster_home = os.getenv("DAGSTER_HOME", "./")
-dagster_home_absolute = os.path.abspath(dagster_home)
-os.environ["DAGSTER_HOME"] = dagster_home_absolute
-
-# Infer Dagster GraphQL API URL from environment variable or default to localhost
-dagster_host = os.getenv("DAGSTER_HOST", "http://localhost")
-dagster_port = os.getenv("DAGSTER_PORT", "3000")
-dagster_graphql_url = f"{dagster_host}:{dagster_port}/graphql"
-
-print(f"DAGSTER_HOME resolved to: {os.environ['DAGSTER_HOME']}")
-print(f"Dagster GraphQL API URL: {dagster_graphql_url}")
-
-
-# Configure logging
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 log = logging.getLogger("fasthtml")
 
 app,rt = fast_app(debug=False)
 
 
-@rt('/')
-def get():
+@app.get('/')
+def home():
 
     # get enabled jobs from Dagster
-    enabled_jobs = get_enabled_dagster_jobs(dagster_graphql_url)
+    enabled_jobs = get_enabled_dagster_jobs()
 
     figs = []
 
@@ -59,19 +38,16 @@ def get():
 
                 metric_df = df[df["metric_name"] == metric_name]
                 # fig = plot_time_series(metric_df, metric_name)
-
-                div = Div(
-                    P(f"Metric Batch: {metric_batch}"),
-                    P(f"Metric Name: {metric_name}"),
-                    Table(metric_df.head().to_html()),
-                )
-                figs.append(div)
+                figs.append(Div(Table(metric_df.columns, metric_df.head().to_html())))
 
         else:
             log.debug(f"job {metric_batch}_ingest is not enabled.")
 
-    return figs
+    return (
+        Title("Anomstack"), 
+        figs
+        )
 
 
 
-serve(host="0.0.0.0", port=5002)
+serve()
