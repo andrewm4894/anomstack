f42a899e087e47e78278ecfe45cbffe88e08c3ba
andrewm4894 <andrewm4894@gmail.com>
2025-02-16 20:40:29 +0000

    Update dashboard and requirements
    - Remove fh-plotly from requirements
    - Add MonsterUI to requirements
    - Refactor fasthtml-dashboard.py to use MonsterUI components
    - Simplify dashboard layout with new styled components
    - Remove ClickHouse database specification from python_ingest_simple example

--- dashboard/fasthtml-dashboard.py
+++ dashboard/fasthtml-dashboard.py
@@ -1,7 +1,9 @@
 import logging
 
-from fasthtml.common import Title, fast_app, serve
-from fh_plotly import plotly2fasthtml, plotly_headers
+from fasthtml.common import *  # Bring in all of fasthtml
+import fasthtml.common as fh  # Used to get unstyled components
+from monsterui.all import *  # Bring in all of monsterui, including shadowing fasthtml components with styled components
+from fasthtml.svg import *
 from utils import get_enabled_dagster_jobs, plot_time_series
 
 from anomstack.config import specs
@@ -10,25 +12,44 @@ from anomstack.sql.read import read_sql
 
 log = logging.getLogger("fasthtml")
 
-app, rt = fast_app(hdrs=(plotly_headers,))
+app, rt = fast_app(hdrs=Theme.blue.headers(), debug=True, log=log)
 
 
-@app.get('/')
-def home():
+def InfoCard(title, value, change):
+    return Card(H3(value), P(change, cls=TextPresets.muted_sm), header=H4(title))
+
+
+@rt
+def index():
 
     # get enabled jobs from Dagster
     enabled_jobs = get_enabled_dagster_jobs()
 
+    print(f"{enabled_jobs=}")
+
     figs = []
+
+    rev = InfoCard("Total Revenue", "$45,231.89", "+20.1% from last month")
+    sub = InfoCard("Subscriptions", "+2350", "+180.1% from last month")
+    sal = InfoCard("Sales", "+12,234", "+19% from last month")
+    act = InfoCard("Active Now", "+573", "+201 since last hour")
+
+    info_card_data = [
+        ("Total Revenue", "$45,231.89", "+20.1% from last month"),
+        ("Subscriptions", "+2350", "+180.1% from last month"),
+        ("Sales", "+12,234", "+19% from last month"),
+        ("Active Now", "+573", "+201 since last hour"),
+    ]
+
+    top_info_row = Grid(*[InfoCard(*row) for row in info_card_data])
 
     # loop through metric batches
     for i, metric_batch in enumerate(specs, start=1):
 
-        if i > 3:
-            break
-
         # check if the job is enabled
-        if f'{metric_batch}_ingest' in enabled_jobs:
+        if f"{metric_batch}_ingest" in enabled_jobs:
+
+            print(f"job {metric_batch}_ingest is enabled.")
 
             spec = specs[metric_batch]
             sql = render("dashboard_sql", spec, params={"alert_max_n": 90})
@@ -37,18 +58,22 @@ def home():
 
             for metric_name in df["metric_name"].unique():
 
+                print(f"metric_name: {metric_name}")
+
                 metric_df = df[df["metric_name"] == metric_name]
-                fig = plotly2fasthtml(plot_time_series(metric_df, metric_name))
+                fig = Safe(
+                        plot_time_series(metric_df, metric_name).to_html(
+                            include_plotlyjs=True,
+                            full_html=False,
+                            config={"displayModeBar": False},
+                        )
+                    )
                 figs.append(fig)
 
         else:
-            log.debug(f"job {metric_batch}_ingest is not enabled.")
+            print(f"job {metric_batch}_ingest is not enabled.")
 
-    return (
-        Title("Anomstack"),
-        figs
-        )
+    return (Title("Anomstack"), top_info_row, figs)
 
 
-
-serve()
+serve(host="localhost", port=5003)
