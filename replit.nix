1f8f86d72a7920d6b4611d66fd10ceeecf6db7e4
Andrew Maguire <andrewm4894@gmail.com>
2025-03-15 20:16:20 +0000

    add some replit files

---
+++ replit.nix
@@ -0,0 +1,17 @@
+{pkgs}: {
+  deps = [
+    pkgs.libyaml
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.pkg-config
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
+    pkgs.glibcLocales
+    pkgs.openssl
+  ];
+}
