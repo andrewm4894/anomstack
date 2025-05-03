b5fb50461f8597542b228d4b0591b136f0a6d984
andrewm4894 <andrewm4894@gmail.com>
2025-05-03 23:34:54 +0100

    dev

--- metrics/defaults/python/preprocess.py
+++ metrics/defaults/python/preprocess.py
@@ -24,13 +24,17 @@ def preprocess(
         freq_agg (str): The aggregation method for resampling.
     """
 
+    logger = get_dagster_logger()
+
+    logger.info(f"preprocess df shape: {df.shape}")
     X = (
         df.sort_values(by=["metric_timestamp"])
         .reset_index(drop=True)
         .set_index("metric_timestamp")
     )
+    logger.info(f"preprocess X shape: {X.shape}")
     X = X[["metric_value"]]
-
+    logger.info(f"preprocess X shape: {X.shape}")
     if freq is not None:
         if freq_agg == "mean":
             X = X.resample(freq).mean()
@@ -42,18 +46,20 @@ def preprocess(
 
     if diff_n > 0:
         X["metric_value"] = X["metric_value"].diff(periods=diff_n).dropna()
+    logger.info(f"preprocess X shape: {X.shape}")
 
     if smooth_n > 0:
         X["metric_value"] = X["metric_value"].rolling(window=smooth_n).mean().dropna()
-
+    logger.info(f"preprocess X shape: {X.shape}")
     if lags_n > 0:
         for lag in range(1, lags_n + 1):
             X[f"lag_{lag}"] = X["metric_value"].shift(lag)
-
+    logger.info(f"preprocess X shape: {X.shape}")
     if shuffle:
         X = X.sample(frac=1)
-
+    logger.info(f"preprocess X shape: {X.shape}")
+    logger.info(X)
     if dropna:
         X = X.dropna()
-
+    logger.info(f"preprocess X shape: {X.shape}")
     return X
